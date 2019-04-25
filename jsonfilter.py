b07e89458bfcc5e23cce61bd00452281cafbc3d1
Jane Chen <jchen785@wisc.edu>
2019-04-24 21:17:05 -0500

    tomcat filter

--- jsonfilter.py
+++ jsonfilter.py
@@ -1,6 +1,22 @@
 import sys
 import json
 
+def extract_info(obj, keys, sensitive, temp_dict, dealingFunc, flat_dict):
+	if isinstance(obj, dict):
+		for k, v in obj.items():
+			if isinstance(v, (dict, list)):
+				extract_info(v, keys, sensitive, temp_dict, dealingFunc, flat_dict)
+			else:
+				flat_dict[k] = v
+				if k in keys:
+					if k in sensitive:
+						temp_dict[k] = dealingFunc[sensitive.index(k)](v)
+					else:
+						temp_dict[k] = v
+	elif isinstance(obj, list):
+		for item in obj:
+			extract_info(item, keys, sensitive, temp_dict, dealingFunc, flat_dict)
+
 def checkSecurePass(password):
 	for letter in password:
 		hasLower = letter.islower()
@@ -20,6 +36,9 @@ def dealPass(password):
 def dealRename(command):
 	return command.split()[0] + " " + "******"
 
+def dealContext(secret):
+	return "******"
+
 def main():
 	app_type = ["redis", "mongodb", "nginx", "spark", "tomcat", "wordpress", "joomla"]
 
@@ -28,7 +47,7 @@ def main():
 	nginxDefault = ["server_tokens", "ssl_protocols", "listen", ]
 	sparkDefault = ["spark.blockManager.port", "spark.driver.blockManager.port", "spark.ssl.enabled", "spark.ssl.[namespace].port", 
 	"spark.ssl.enabledAlgorithms", "spark.ssl.needClientAuth"] 
-	tomcatDefault = ["Server"]
+	tomcatDefault = ["@shutdown", "@SSLEnabled", "web-resource-name", "url-pattern", "transport-guarantee"]
 	wordpressDefault = []
 	joomlaDefault = []
 	defaultList = [redisDefault, mongodbDefault, nginxDefault, sparkDefault, tomcatDefault, wordpressDefault, joomlaDefault]
@@ -36,15 +55,24 @@ def main():
 	mongodbSensitive = []
 	nginxSensitive = ["","","ssl_ciphers", "ssl_certificate", "ssl_certificate_key"]
 	sparkSensitive = ["","","spark.ssl.trustStore", "spark.ssl.trustStorePassword"]
-	tomcatSensitive = []
+	tomcatSensitive = ["web-resource-name", "transport-guarantee"]
 	wordpressSensitive = []
 	joomlaSensitive = []
 	sensitiveList = [redisSensitive, mongodbSensitive, nginxSensitive, sparkSensitive, tomcatSensitive, wordpressSensitive, joomlaSensitive]
-	dealingFunc = [dealPass, dealRename]
+	redisDealingFunc = [dealPass, dealRename]
+	mongodbDealingFunc = []
+	nginxDealingFunc = []
+	sparkDealingFunc = []
+	tomcatDealingFunc = [dealContext, dealContext]
+	wordpressDealingFunc = []
+	joomlaDealingFunc = []
+	dealingFuncList = [redisDealingFunc, mongodbDealingFunc, nginxDealingFunc, sparkDealingFunc, tomcatDealingFunc, wordpressDealingFunc, joomlaDealingFunc]
 
 	app_dict = {}
 	temp_dict = {}
 	sensitive_dict = {}
+	dealingFunc_dict = {}
+	flat_dict = {}
 
 	if sys.argv[1] not in app_type:
 		print("Application type not found.")
@@ -53,23 +81,24 @@ def main():
 	for i in range(len(app_type)):
 		app_dict[app_type[i]] = defaultList[i]
 		sensitive_dict[app_type[i]] = sensitiveList[i]
+		dealingFunc_dict[app_type[i]] = dealingFuncList[i]
 
 	selectedKey = app_dict[sys.argv[1]]
 	sensitiveInfo = sensitive_dict[sys.argv[1]]
+	dealingFunc = dealingFunc_dict[sys.argv[1]]
 
 	json_file_name = "./conftest/" + sys.argv[1] + "-conf.json"
+	json_file_output_name = "./conftest/" + sys.argv[1] + "-conf-selected.json"
 
 	with open(json_file_name) as f:
 		data = json.load(f)
-		for element in selectedKey:
-			try:
-				if element in sensitiveInfo:
-					temp_dict[element] = dealingFunc[sensitiveInfo.index(element)](data[element])
-				else:
-					temp_dict[element] = data[element]
-			except:
-				continue
-	with open("./conftest/server-conf-selected.json","w") as target:
+		extract_info(data, selectedKey, sensitiveInfo, temp_dict, dealingFunc, flat_dict)
+
+	# for e in flat_dict:
+	# 	print(e)
+	# 	print(flat_dict[e])
+
+	with open(json_file_output_name,"w") as target:
 		json.dump(temp_dict,target)
 
 if __name__ == "__main__":
